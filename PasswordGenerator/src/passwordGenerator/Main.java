3eed06ace725c486b5823cbc2f2e1af6297f3b3f
nrk25 <coderravikrishna@gmail.com>
2023-08-19 15:58:57 +0530

    project completed

--- PasswordGenerator/src/passwordGenerator/Main.java
+++ PasswordGenerator/src/passwordGenerator/Main.java
@@ -1,8 +1,13 @@
 package passwordGenerator;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+    public static final Scanner keyboard = new Scanner(System.in);
     public static void main(String[] args) {
 	// write your code here
+        Generator generator = new Generator(keyboard);
+        generator.mainLoop();
+        keyboard.close();
     }
 }
